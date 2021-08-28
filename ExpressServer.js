b3b4f3ee62476b1cf3c49a389d41d3eb48ea974b
tdinh6 <tdinh6@uncc.edu>
2021-08-27 22:20:39 -0400

    Simple Express Server

---
+++ ExpressServer.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const app = express()
+
+const port = 3000; 
+
+app.get('/', (req, res) =>{
+    res.send("Hello!")
+})
+
+app.listen(port, () => {
+    console.log('The server is running on this port ', port)
+})
